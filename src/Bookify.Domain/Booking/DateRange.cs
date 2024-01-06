dc3d300ecc94484d515d8870df74b9313311e35f
Rafael Braga <devrbraga@gmail.com>
2024-01-06 19:09:58 -0400

    Booking entity

---
+++ src/Bookify.Domain/Booking/DateRange.cs
@@ -0,0 +1,26 @@
+﻿namespace Bookify.Domain.Booking;
+
+public record DateRange
+{
+    private DateRange()
+    {
+    }
+
+    public DateOnly Start { get; init; }
+    public DateOnly End { get; init; }
+    public int LengthInDays => End.DayNumber - Start.DayNumber;
+
+    public static DateRange Create(DateOnly start, DateOnly end)
+    {
+        if (start > end)
+        {
+            throw new ApplicationException("Start date precedes start date");
+        }
+
+        return new DateRange
+        {
+            Start = start,
+            End = end
+        };
+    }        
+}
