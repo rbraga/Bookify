ff155ea88af5c1dee7dfab7aee19a962bec6cfc8
Rafael Braga <devrbraga@gmail.com>
2024-01-08 23:12:51 -0400

    abstractions for CQRS

---
+++ src/Bookify.Application/Abstractions/IQuery.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions;
+
+public interface IQuery<TResponse> : IRequest<Result<TResponse>>
+{
+
+}
---
+++ src/Bookify.Application/Abstractions/IQueryHandle.cs
@@ -0,0 +1,9 @@
+﻿using Bookify.Domain.Abstractions;
+using MediatR;
+
+namespace Bookify.Application.Abstractions;
+
+public interface IQueryHandle<TQuery, TResponse> : IRequestHandler<TQuery, Result<TResponse>>
+    where TQuery : IQuery<TResponse>
+{
+}
