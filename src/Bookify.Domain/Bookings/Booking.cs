82b3b420e38257e18bbcd13bb7a911f15c5f16d7
Rafael Braga <devrbraga@gmail.com>
2024-01-25 19:10:01 -0400

    defining domain event layer

--- src/Bookify.Domain/Bookings/Booking.cs
+++ src/Bookify.Domain/Bookings/Booking.cs
@@ -1,6 +1,6 @@
 ﻿using Bookify.Domain.Abstractions;
 using Bookify.Domain.Apartments;
-using Bookify.Domain.Booking.Events;
+using Bookify.Domain.Bookings.Events;
 using Bookify.Domain.Shared;
 
 namespace Bookify.Domain.Bookings;
