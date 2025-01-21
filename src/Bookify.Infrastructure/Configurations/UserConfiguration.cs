89ec068325778abf2c8a418edf7c9d3708c8c5a5
Rafael Braga <devrbraga@gmail.com>
2025-01-20 22:38:33 -0400

    Add entity configurations for Apartment, Booking, and User
    Updated ApplicationDbContext to include OnModelCreating method.
    Added ApartmentConfiguration to configure Apartment entity.
    Added BookingConfiguration to configure Booking entity.
    Added ReviewConfiguration with NotImplementedException.
    Added UserConfiguration to configure User entity with unique Email.

---
+++ src/Bookify.Infrastructure/Configurations/UserConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookify.Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Bookify.Infrastructure.Configurations;
+
+internal sealed class UserConfiguration:  IEntityTypeConfiguration<User>
+{
+    public void Configure(EntityTypeBuilder<User> builder)
+    {
+        builder.ToTable("users");
+
+        builder.HasKey(t => t.Id);
+
+        builder.Property(t => t.FirstName)
+            .HasMaxLength(200)
+            .HasConversion(firstName => firstName.Value, value => new FirstName(value));
+
+        builder.Property(t => t.LastName)
+            .HasMaxLength(200)
+            .HasConversion(lastName => lastName.Value, value => new LastName(value));
+
+        builder.Property(t => t.Email)
+            .HasMaxLength(400)
+            .HasConversion(email => email.Value, value => new Domain.Users.Email(value));
+
+        builder.HasIndex(user => user.Email).IsUnique();
+    }
+}
+
